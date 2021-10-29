fc41ad8fea0aa592a3605f9bdf8f135585534da6
Mariano <marianopablo97@hotmail.com>
2021-10-29 19:07:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['pymomorphic', 'pymomorphic3'],
+     packages=['pymomorphic3'],
      package_dir={'': 'src'},
      install_requires=['csv', 'random', 'rospkg', 'numpy']
 )
